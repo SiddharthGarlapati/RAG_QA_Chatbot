1f5e78a1c9c2364e1df21b1b20d2ec38488e32b8
Sid <sidgarlapati@gmail.com>
2026-01-27 19:16:16 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-st.secrets["OPENAI_API_KEY"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 
 embeddings = OpenAIEmbeddings(model= "text-embedding-3-large")
